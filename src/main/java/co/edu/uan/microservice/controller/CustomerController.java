4159c143df63e385ec1cbd08470c711396f22a58
Diego Malagón <diegomalagonh@gmail.com>
2019-03-22 18:01:07 -0500

    Spring Boot configurado

---
+++ src/main/java/co/edu/uan/microservice/controller/CustomerController.java
@@ -0,0 +1,12 @@
+package co.edu.uan.microservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class CustomerController {
+	@RequestMapping("/")
+    public String index() {
+        return "Bienvenido al microservicio de gestión de clientes!";
+    }
+}
