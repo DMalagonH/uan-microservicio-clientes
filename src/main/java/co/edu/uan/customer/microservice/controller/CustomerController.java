601ae2247984b66c5e1921d3227682d75469c527
Diego Malagón <diegomalagonh@gmail.com>
2019-03-22 19:24:46 -0500

    Agregar /health endpoint

--- src/main/java/co/edu/uan/customer/microservice/controller/CustomerController.java
+++ src/main/java/co/edu/uan/customer/microservice/controller/CustomerController.java
@@ -1,12 +1,24 @@
 package co.edu.uan.customer.microservice.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
+@RequestMapping({ "/" })
+@CrossOrigin(origins = "*")
 public class CustomerController {
+	
 	@RequestMapping("/")
     public String index() {
         return "Bienvenido al microservicio de gestión de clientes!";
     }
+	
+	@GetMapping(value = "/health")
+	public ResponseEntity<String> health() {
+		return new ResponseEntity<>("OK", HttpStatus.OK);
+	}
 }
